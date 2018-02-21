8015de57e48fe723672a374db6122776268287b8
JShi0100 <36395606+JShi0100@users.noreply.github.com>
2018-02-21 10:28:59 -0500

    Update FirstProg.java

--- 03/FirstProg.java
+++ 03/FirstProg.java
@@ -1,7 +1,7 @@
 public class FirstProg
 {
-	public static void main( String[] args )
+	public static void main(String[] args)
 	{
-		System.out.println( "Mr. Mitchell is cool." );
+		System.out.println("Mr. Mitchell is cool.");
 	}
 }
