65b245c19409256b83568bb9673ba05187985e7b
JShi0100 <36395606+JShi0100@users.noreply.github.com>
2018-02-21 10:29:46 -0500

    Update FirstProgShaweon.java

--- 03/FirstProgShaweon.java
+++ 03/FirstProgShaweon.java
@@ -1,7 +1,7 @@
 public class FirstProgShaweon
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello shaweonette, welcome to Josie's amazing world. This is where you will be able to have your magical dreams of riding unicorns and jumping over rainbows! Enjoy your stay in Josieville.. :) " );
+        System.out.println("Hello shaweonette, welcome to Josie's amazing world. This is where you will be able to have your magical dreams of riding unicorns and jumping over rainbows! Enjoy your stay in Josieville.. :) " );
     }
 }
